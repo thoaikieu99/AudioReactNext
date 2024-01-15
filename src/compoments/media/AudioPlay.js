7dedbcb21e7a9a46b7333bffa21e0a6844f023bd
thoaikieu99 <nogamenolife1508@gmail.com>
2024-01-15 13:06:44 +0700

    fix player

--- src/compoments/media/AudioPlay.js
+++ src/compoments/media/AudioPlay.js
@@ -5,13 +5,59 @@ import "react-h5-audio-player/lib/styles.css";
 import PerfectScrollbar from "react-perfect-scrollbar";
 import "react-perfect-scrollbar/dist/css/styles.css";
 import { Container, Row } from "react-bootstrap";
+import { useCookies } from "react-cookie";
 
 const AudioPlay = (props) => {
+  const STARTTIME = "startime-" + props.audio.id;
+  const SPEED = "startspeed-" + props.audio.id;
+  const INDEXAU = "track-" + props.audio.id;
+  const MAXAGE = 7 * 24 * 3600;
+
+  const [cookies, setCookie] = useCookies();
   const playlist = props.audio.link_audio.split("<br>");
 
-  const [speed, setSpeed] = useState(1);
+  const [covertTime, setCovertTime] = useState();
+  //setData cookies
+  const [cookieTime, setCookieTime] = useState();
+  const [cookieIndex, setCookieIndex] = useState();
+  const [cookieSpeed, setCookieSpeed] = useState();
+  const [isclick, setIsclick] = useState(false);
 
+  //data hien tai
+  const [speed, setSpeed] = useState(1);
+  const [checkCook, setCheckCook] = useState(false);
   const [currentTrack, setTrackIndex] = useState(0);
+  const buttonRef = useRef(null);
+
+  useEffect(() => {
+    setCookieIndex(cookies[INDEXAU]);
+    setCookieTime(cookies[STARTTIME]);
+    setCookieSpeed(cookies[SPEED]);
+    if (cookies[STARTTIME]) {
+      setCheckCook(true);
+      const coverTime = (totalSeconds) => {
+        let hours = Math.floor(totalSeconds / 3600);
+        totalSeconds %= 3600;
+        let minutes = Math.floor(totalSeconds / 60);
+        let seconds = totalSeconds % 60;
+        let time = `${hours < 10 ? "0" + hours : hours}:${
+          minutes < 10 ? "0" + minutes : minutes
+        }:${seconds < 10 ? "0" + seconds : seconds}`;
+        return time;
+      };
+      setCovertTime(coverTime(cookies[STARTTIME]));
+    }
+  }, []);
+
+  useEffect(() => {
+    buttonRef.current.audio.current.playbackRate = speed;
+  }, [buttonRef, speed]);
+
+  useEffect(() => {
+    const collection = document.getElementsByClassName("audio active");
+    collection[0].scrollIntoView(false);
+  }, [currentTrack]);
+
   const handleClickPrevious = () => {
     setTrackIndex((currentTrack) => (currentTrack > 1 ? currentTrack - 1 : 0));
   };
@@ -26,27 +72,44 @@ const AudioPlay = (props) => {
     );
   };
   const saveLocal = (currentTime, scr) => {
-    console.log(parseInt(currentTime), "ashdghas", scr);
+    let time = parseInt(currentTime);
+    setCookie(STARTTIME, time, { maxAge: MAXAGE });
+    setCookie(SPEED, speed, { maxAge: MAXAGE });
+    setCookie(INDEXAU, currentTrack, { maxAge: MAXAGE });
   };
-  const buttonRef = useRef(null);
-  useEffect(() => {
-    buttonRef.current.audio.current.playbackRate = speed;
-  }, [buttonRef, speed]);
 
-  const ListAudio = playlist.map((value, index) => {
+  const hangeClick = (ev) => {
+    let id = parseInt(ev.target.id);
+    setTrackIndex(id);
+  };
+  let ListAudio = playlist.map((value, index) => {
     return (
       <div
         key={"audio-" + index}
+        id={index}
         className={`audio ${currentTrack === index ? "active" : ""}`}
+        onClick={hangeClick}
       >
         Tập {index + 1}
       </div>
     );
   });
-  const loaded = (cur) => {
-    buttonRef.current.audio.current.playbackRate = speed;
+
+  const loaded = () => {
+    let audio = buttonRef.current.audio.current;
+    audio.playbackRate = speed;
+    if (isclick) {
+      setIsclick(true);
+      audio.currentTime = parseInt(cookieTime);
+      audio.play();
+    }
   };
 
+  const ngheTiepClick = () => {
+    setIsclick(true);
+    setTrackIndex(cookieIndex);
+    setSpeed(cookieSpeed);
+  };
   const ll = (
     <AudioPlayer
       header={<h3 style={{ textAlign: "center" }}>{props.audio.title}</h3>}
@@ -84,6 +147,14 @@ const AudioPlay = (props) => {
       <div className="AudioPlay col-12 col-sm-7 col-md-6 rounded-3 shadow-lg bg-body ">
         <div className="list">
           {ll}
+          <div
+            className="ngheTiep"
+            style={checkCook ? { display: "block" } : { display: "none" }}
+          >
+            <span className="tii" onClick={ngheTiepClick}>
+              Nghe Tiếp {covertTime}
+            </span>
+          </div>
           <PerfectScrollbar>
             <Container>
               <Row className="justify-content-md-center">
